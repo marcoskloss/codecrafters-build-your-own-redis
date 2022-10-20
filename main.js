3acce8e36408214daf5f6f14f9e4c470d9710bb5
Marcos Kloss <marcoskloss2001@gmail.com>
2022-10-20 14:43:10 -0300

    stage 6 completed

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const net = require('net');
 const protocol = require('./protocol');
 
+const db = new Map();
+
 const isPingCommand = (command) => {
     if (Array.isArray(command)) {
         return command.map((str) => str.toLowerCase()).includes('ping');
@@ -19,6 +21,25 @@ const handleEchoCommand = (command, socket) => {
     socket.write(protocol.encodeRespBulkString(command[1]));
 };
 
+const isSetCommand = (command) => 
+    Array.isArray(command) && command[0].toLowerCase() === 'set';
+
+const handleSetCommand = (command, socket) => {
+    const [_, key, value] = command;
+    db.set(key, { value })
+    socket.write(protocol.encodeRespString('OK'))
+}
+
+const isGetCommand = (command) => 
+    Array.isArray(command) && command[0].toLowerCase() === 'get';
+
+const handleGetCommand = (command, socket) => {
+    const [_, key] = command;
+    const object = db.get(key) ?? {};
+    const value = object.value ?? null;
+    socket.write(protocol.encodeRespBulkString(value));
+}
+
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const message = data.toString()
@@ -30,6 +51,14 @@ const server = net.createServer((socket) => {
 
         if (isEchoCommand(decoded)) {
             return handleEchoCommand(decoded, socket);
+        }
+
+        if (isSetCommand(decoded)) {
+            return handleSetCommand(decoded, socket)
+        }
+
+        if (isGetCommand(decoded)) {
+            return handleGetCommand(decoded, socket);
         }
   });
 });
